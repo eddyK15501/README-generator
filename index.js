e7054d24e3a153f670a44fd3ca4444c74d98ac0a
eddyK15501 <itgma1231@gmail.com>
2023-08-02 00:21:42 -0400

    added function for async/await fs.promises.writeFile

--- index.js
+++ index.js
@@ -1,52 +1,64 @@
 const inquirer = require('inquirer')
-const fs = require('fs')
+const { writeFile } = require('fs').promises
 
-console.log("This is a README.md file generator. Please enter the following:")
-
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Title:'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Description:'
-    },
-    {
-        type: 'input',
-        name: 'TOC',
-        message: 'Table Of Contents:'
-    },
-    {
-        type: 'input',
-        name: 'install',
-        message: 'Installations:'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Usage:'
-    },
-    // {
-    //     type: 'input',
-    //     name: 'license',
-    //     message: 'License:'
-    // }
-    {
-        type: 'input',
-        name: 'contribute',
-        message: 'How to Contribute:'
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Testing Instructions:'
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        message: 'Questions:'
+const writeToDist = async (content) => {
+    try {
+        await writeFile('./dist/README.md', content)
+    } catch (error) {
+        console.log(error)
     }
-])
+}
+
+const promptInquirer = () => {
+    console.log("This is a README.md file generator. Please enter the following:")
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Title:'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Description:'
+        },
+        {
+            type: 'input',
+            name: 'contents',
+            message: 'Table Of Contents:'
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Installations:'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Usage:'
+        },
+        // {
+        //     type: 'input',
+        //     name: 'license',
+        //     message: 'License:'
+        // }
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'How to Contribute:'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Testing Instructions:'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Questions:'
+        }
+    ]).then(data => console.log(data))
+}
+
+startInquiry()
